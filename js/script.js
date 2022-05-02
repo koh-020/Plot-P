9782035f31db332158a2ad7e233da121393d97f8
koh-020 <97607232+koh-020@users.noreply.github.com>
2022-05-03 00:26:56 +0900

    add bizier

--- js/script.js
+++ js/script.js
@@ -28,3 +28,41 @@
 $('.start').on('click', function(){
   $('.circle').css('background-color', 'red')
 });
+
+
+
+
+
+// ビジェ曲線のアニメーション
+
+var getPointQuadraticCurve = function(t, x1, y1, x2, y2, x3, y3) {
+  var tp = 1 - t;
+  var x = t * t * x3 + 2 * t * tp * x2 + tp * tp * x1;
+  var y = t * t * y3 + 2 * t * tp * y2 + tp * tp * y1;
+
+  return { x : x, y : y };
+};
+
+$(function() {
+  var $point = $('.sample .point');
+
+  var startPos = { x : 0,   y : 180 }; // 始点（座標）
+  var endPos   = { x : 180, y : 0 };   // 終点（座標）
+  var ctrlPos  = { x : 180, y : 180 }; // 制御点（座標）
+
+  var startAnimate = function() {
+      $point.animate({ count : 1 }, {
+          duration : 1000,
+          step     : function(current) {
+              var point = getPointQuadraticCurve(current, startPos.x, startPos.y, ctrlPos.x, ctrlPos.y, endPos.x, endPos.y);
+              $point.css({ left : point.x, top : point.y });
+          },
+          complete : function() {
+              this.count = 0;
+              startAnimate(); // 繰り返し
+          }
+      });
+  };
+
+  startAnimate();
+});
